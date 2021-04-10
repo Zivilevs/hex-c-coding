f0a34593b531c0238351738fbeac11316fbaf6de
Zivilevs <zivileval@yahoo.com>
2021-04-10 15:14:51 -0300

    printsquares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+/**
+ * print_square - a function that prints square followed by new line.
+ *
+ * @size: integer
+ * Return: integer
+ */
+
+#include "holberton.h"
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i = 0;
+
+		while (i < size)
+		{
+			int j = 1;
+
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
