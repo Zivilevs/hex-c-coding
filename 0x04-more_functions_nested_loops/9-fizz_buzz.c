3d5318a74c2bfddff79279de706f1c27ec70be36
Zivilevs <zivileval@yahoo.com>
2021-04-10 17:06:06 -0300

    update for last space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,13 @@
 /**
  * main - fizz buzz
+ * fizz_buzz - fizz buzz
+ *@i: integer
  * Return: always 0
  */
 
 #include <stdio.h>
+
+void fizz_buzz(int i);
 
 int main(void)
 {
@@ -11,26 +15,43 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i < 100)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
+			fizz_buzz(i);
+			printf(" ");
 
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", i);
+			fizz_buzz(i);
+			printf("\n");
 		}
 		i++;
 	}
-	printf("\n");
 	return (0);
 }
 
+/**
+ * fizz_buzz - fizz buzz
+ *@i: integer
+ */
+
+void fizz_buzz(int i)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d", i);
+	}
+}
