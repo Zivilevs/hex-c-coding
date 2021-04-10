44ffeb292ae2ca8d26ef8242cc79f133d552fe48
Zivilevs <zivileval@yahoo.com>
2021-04-10 16:20:40 -0300

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+/**
+ * main - fizz buzz
+ * Return: always 0
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int i  = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
