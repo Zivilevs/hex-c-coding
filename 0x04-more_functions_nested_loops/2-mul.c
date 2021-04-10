97cbf3df2238b654129f10dab2738104b121b53e
Zivilevs <zivileval@yahoo.com>
2021-04-09 23:41:22 -0300

    function for multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul  - multiplies 2 digits.
+ *@a: integer
+ *@b: integer
+ * Return: integer
+ */
+
+#include "holberton.h"
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
