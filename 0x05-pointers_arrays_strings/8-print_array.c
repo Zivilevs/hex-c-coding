f7584690725ae7a0622e4b6ea69052de400fc5c2
Zivilevs <zivileval@yahoo.com>
2021-04-12 23:41:59 -0300

    error raise

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,6 +16,14 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		i != (n - 1) ? printf(", ") : printf("\n");
+		if (i != (n - 1))
+		{
+		       printf(", ");
+		}
+	}
+	
+	if (n >= 0)
+	{
+		printf("\n");
 	}
 }
