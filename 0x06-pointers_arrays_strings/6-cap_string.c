5f35dc608561548336a96eb053283490f7eae2aa
Zivilevs <zivileval@yahoo.com>
2021-04-18 22:27:17 -0300

    every word capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+/**
+ * cap_string - function that capitalize all words of a string.
+ * @o: a pointer to a char
+ * Return: char
+ * Auth: Zivile V. Silveira
+ */
+
+#include "holberton.h"
+
+char *cap_string(char *o)
+{
+	int i;
+
+	i = 0;
+
+	for (; *(o + i) != '\0'; i++)
+	{
+		if ((*(o + i) == ',') || (o[i] ==  ';') || (o[i] == '.') || (o[i] == '!') || (o[i] == '?')
+					|| (o[i] == '"') || (o[i] == '(') || (o[i] == ')') || (o[i] == '{') || (o[i] == '}')
+					|| (o[i] == '\t') || (o[i] == ' ') || (o[i] == '\n'))
+		{
+			i++;
+			if (o[i] >= 'a' && o[i] <= 'z')
+			{
+				o[i] = o[i] - 32;
+			}
+		}
+	}
+	return (o);
+}
