c2798d018bee2b6985b7622f42945ac13092c163
Zivilevs <zivileval@yahoo.com>
2021-05-05 09:37:58 -0300

    style update

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -1,6 +1,7 @@
 /**
  * _strlen_recursion - function that returnes the length of a string.
  * @s: a pointer to a char
+ * Return: length - integer
  * *s is value of first element in string, if it is \0, then recursive
  * function calls  value of 0, else every time it call itself it
  * returns 1 and accumulate the stack one by one.
