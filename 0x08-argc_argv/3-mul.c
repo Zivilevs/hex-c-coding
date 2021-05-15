13e9fec4b5d816a47e1cdc4288f7a9bd167858d4
Zivilevs <zivileval@yahoo.com>
2021-05-15 16:48:43 -0300

    Multiply arguments

---
+++ 0x08-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+/**
+ * main - multiplies 2 numbers.
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: integer
+ * Auth: Zivile V. Silveira
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	int result;
+
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+	return (0);
+}
--- 0x08-argc_argv/README.md
+++ 0x08-argc_argv/README.md
@@ -5,3 +5,5 @@ Use of global variables is not allowed.
 1.  A program that prints its name, followed by a new line.
 2.  A program that prints number of arguments passed into it.
 3.  A program thats prints all passed arguments and new line.
+4.  A program that multiplies two numbers.
+5.   A program that adds positive numbers.
