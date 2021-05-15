cc877ef272982cbb151e6d95a8bb98e4f5c3ff8a
Zivilevs <zivileval@yahoo.com>
2021-05-15 15:53:17 -0300

    Print program name

---
+++ 0x08-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+/**
+ * main - main function
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: integer
+ * Auth: Zivile V. Silveira
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
+
---
+++ 0x08-argc_argv/README.md
@@ -0,0 +1,6 @@
+# argc, argv
+
+Use of global variables is not allowed.
+
+1.  A program that prints its name, followed by a new line.
+2. 
