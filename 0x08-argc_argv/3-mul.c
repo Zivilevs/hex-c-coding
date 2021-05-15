d3b55d1ae8709892b65f379a67ebb4b25e05cc8a
Zivilevs <zivileval@yahoo.com>
2021-05-15 16:54:52 -0300

    Declare varaible in the begining

--- 0x08-argc_argv/3-mul.c
+++ 0x08-argc_argv/3-mul.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char **argv)
 {
+	int result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (0);
 	}
-
-	int result;
 
 	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
