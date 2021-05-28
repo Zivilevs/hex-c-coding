6c83b986d6cc3c86600b92709026f0c5be1ddc62
Zivilevs <zivileval@yahoo.com>
2021-05-28 12:34:39 -0300

    pointer to pointers

---
+++ 0x09-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+/**
+ * alloc_grid - function that returns a pointer
+ * to a 2 dimensional array of int.
+ * @width: integer
+ * @height: integer
+ * Return: array
+ * Auth: Zivile V. Silveira
+ */
+
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **arr;
+
+
+	if ((width <= 0) || (height <= 0))
+	{
+		return (NULL);
+	}
+	arr = (int **)malloc(sizeof(int *) *  height);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			arr[i] = (int *)malloc(sizeof(int) * width);
+		}
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				arr[i][j] = 0;
+			}
+		}
+		return (arr);
+	}
+}
--- 0x09-malloc_free/README.md
+++ 0x09-malloc_free/README.md
@@ -3,3 +3,4 @@
 1. A function that creates an array of chars, and initializes it with a specific char.
 2. A function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 3. A function that concatenates two strings.
+4. A function that returns a pointer to a 2 dimensional array of integers.
