0e4e005bdd1f9e51cb0b49efe3d2cc5fb99ef157
Zivilevs <zivileval@yahoo.com>
2021-05-28 12:59:56 -0300

    free array

--- 0x09-malloc_free/3-alloc_grid.c
+++ 0x09-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 	}
 	else
 	{
-		arr = (int **)malloc(sizeof(int *) *  height);
+		arr = (int **)malloc(sizeof(int *) * height);
 		if (arr == NULL)
 		{
 			free(arr);
@@ -35,6 +35,11 @@ int **alloc_grid(int width, int height)
 			for (i = 0; i < height; i++)
 			{
 				arr[i] = (int *)malloc(sizeof(int) * width);
+				if (arr == NULL)
+				{
+					free(arr);
+					return (NULL);
+				}
 			}
 			for (i = 0; i < height; i++)
 			{
