495f2c75ac967b65283152ed786b1cf6a7a1148b
Zivilevs <zivileval@yahoo.com>
2021-07-01 18:42:23 -0300

    4 task

--- 0x0B-more_malloc_free/2-main.c
+++ 0x0B-more_malloc_free/2-main.c
@@ -1,7 +1,8 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void *_calloc(unsigned int, unsigned int);
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -10,25 +11,25 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(int *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
@@ -38,13 +39,17 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char *a;
+	int *a;
+	unsigned int nmemb;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Holberton");
-    strcpy(a + 9, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
+	nmemb = 50;
+	a = _calloc(nmemb, sizeof(*a));
+	if (a == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	simple_print_buffer(a, nmemb);
+	free(a);
+	return (0);
 }
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -54,3 +54,4 @@ char *argstostr(int ac, char **av);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
